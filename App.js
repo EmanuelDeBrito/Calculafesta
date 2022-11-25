aff1471a7af7cf2db8aa1655277835ed1bc078af
1uan3010 <you@example.com>
2022-11-25 18:54:33 +0000

    Calcular festa

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import Calcula from './components/Calcula';
 
 export default function App() {
+	
+	const [inputQtd, setInputQtd] = useState(0);
+	const [qtd, setQtd] = useState(0);
+	
+	function enviaValor(){
+		setQtd(inputQtd);
+	}
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    	<View style={styles.bloco}>
+			<Text style={style.titulo}>Calculadora Festa</Text>
+		</View>
+		  <View style={bloco}>
+			  <Text style={styles.Label}>QTD.Convidades</Text>
+			  <TextInput
+				  style={styles.input}
+				  value={inputQtd}
+				  onChangeText={(value) => setInputQtd(value)}
+			  />
+			  	  <TouchableOpacity style={styles.botao} onPress={enviaValor}>
+			  	  	   <Text style={styles.txtBotao}>Calcular</Text>
+			      </TouchableOpacity>
+		  </View>
+		  <View style={styles.bloco}>
+			  
+		  </View>
     </View>
   );
 }
@@ -13,8 +36,42 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: '#fff'
+    },
+	
+	
+	bloco:{
+		width:'80%',
+		marginLeft:'10%'
+	},
+	
+	
+	titulo:{
+		fontSize:'30'
+	},
+	
+	
+	input:{
+		borderWidth:'1',
+		fontSize:'20',
+		width:'100%'
+	},
+	
+	
+	label:{
+		fontSize:'20'
+	},
+	
+	
+	botao:{
+		width:'100%',
+		backgroundColor:'#000',
+		alignItems:'center'
+	},
+	
+	
+	textBotao:{
+		fontSize:'20',
+		color:'#fff'
+	},
 });
